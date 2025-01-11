d89d6127d94e42831e111773ed644234606ab1a7
Ryu613 <411379453@qq.com>
2025-01-11 18:25:09 +0800

    project init

---
+++ README.md
@@ -0,0 +1,14 @@
+# MyVulkanSamples
+
+整理各种有用的Vulkan示例工程或代码，有些来自书籍有些来自其他地方，主要用于个人消化
+
+## samples
+
+- `modern-cookbook`: 重新总结
+
+## reference
+
+- [`modern-cookbook`](https://github.com/PacktPublishing/The-Modern-Vulkan-Cookbook): from book modern-vulkan-cookbook
+- [`Vulkan-Samples`](https://github.com/KhronosGroup/Vulkan-Samples): Official one
+- [`Vulkan-Samples`](https://github.com/SaschaWillems/Vulkan): Sancha's one
+- [`pbrt-v4`](https://github.com/mmp/pbrt-v4): from book pbr theory and implementation ed4
---
+++ samples/modern-cookbook/include/common.hpp
@@ -0,0 +1,20 @@
+#pragma once
+// 防止与windows.h里的min/max冲突
+#define NOMINMAX
+// 防止符号冲突
+#define VK_NO_PROTOTYPES
+#define VK_USE_PLATFORM_WIN32_KHR
+#include <Volk/volk.h>
+// 可以把Vulkan的枚举名称转成字符串，方便打印日志
+#include <vulkan/vk_enum_string_helper.h>
+
+// 处理vulkan函数的返回值，若有错误就打印
+#define VK_CHK(func)                                                                   \
+  {                                                                                    \
+    const VkResult result = func;                                                      \
+    if (result != VK_SUCCESS) {                                                        \
+      std::cerr << "Error calling function " << #func << " at " << __FILE__ << ":"     \
+                << __LINE__ << ". Result is " << string_VkResult(result) << std::endl; \
+      assert(false);                                                                   \
+    }                                                                                  \
+  }
